b8ba8b16af1628d9be914e3e2c8cab3d54c5c94e
Vegard Nitter Vestad <vegard@vnvestad.com>
2019-02-16 20:35:53 +0100

    Refactor layout file

--- app/src/main/java/com/vegst/memorio/MainActivity.kt
+++ app/src/main/java/com/vegst/memorio/MainActivity.kt
@@ -9,7 +9,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        
         Handler().postDelayed({
             startActivity(Intent(this, LoginActivity::class.java))
             finish()
