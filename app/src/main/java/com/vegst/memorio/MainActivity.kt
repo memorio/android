b396282a42988cd7093882e007f988e43f816ea4
Vegard Nitter Vestad <vegard@vnvestad.com>
2019-02-16 20:21:46 +0100

    Add start-up activity

---
+++ app/src/main/java/com/vegst/memorio/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.vegst.memorio
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Handler().postDelayed({
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        }, 2000)
+    }
+}
